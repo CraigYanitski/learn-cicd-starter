bb856e83ce68332e2ab9bf4ac8df910eb6dabfd3
yanitski <yanitski@ph1.uni-koeln.de>
2025-04-30 18:20:35 -0600

    formatted code

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,62 +6,62 @@ import (
 	"testing"
 )
 
-func TestX(t *testing.T) {
-    type testCase struct {
-        input     string
-        expected  string
-        err       error
-    }
+func TestGettingAPIKey(t *testing.T) {
+	type testCase struct {
+		input    string
+		expected string
+		err      error
+	}
 
-    tests := []testCase{
-        {
-            "ApiKey test_string",
-            "test_string",
-            nil,
-        },
-        {
-            "",
-            "",
-            ErrNoAuthHeaderIncluded,
-        },
-    }
+	tests := []testCase{
+		{
+			"ApiKey test_string",
+			"test_string",
+			nil,
+		},
+		{
+			"",
+			"",
+			ErrNoAuthHeaderIncluded,
+		},
+	}
 
-    failCount := 0
-    passCount := 0
+	failCount := 0
+	passCount := 0
 
-    fmt.Println("\n\nTesting GetAPIKey")
+	fmt.Println("\n\nTesting GetAPIKey")
 
-    for _, test := range tests {
-        fmt.Println("----------------------------------------")
-        fmt.Printf("Getting API key with %v", test.input)
+	for _, test := range tests {
+		fmt.Println("----------------------------------------")
+		fmt.Printf("Getting API key with %v", test.input)
 
-        header := http.Header{}
-        if test.input != "" {
-            header.Add("Authorization", test.input)
-        }
-        result, err := GetAPIKey(header)
+		header := http.Header{}
+		if test.input != "" {
+			header.Add("Authorization", test.input)
+		}
+		result, err := GetAPIKey(header)
 
-        if result != test.expected || err != test.err {
-            failCount++
-            t.Errorf(`
+		if result != test.expected || err != test.err {
+			failCount++
+			t.Errorf(`
 Inputs:        %v
 Expected:      %v
 Error:         %v
 Actual:        %v
 Actual error:  %v
 `, test.input, test.expected, test.err, result, err)
-        } else {
-            passCount++
-            fmt.Printf(`
+		} else {
+			passCount++
+			fmt.Printf(`
 Inputs:        %v
 Expected:      %v
 Error:         %v 
 Actual:        %v
 Actual error:  %v
 `, test.input, test.expected, test.err, result, err)
-        }
-    }
+		}
+	}
 
-    fmt.Println("========================================")
-    fmt.Printf("%d passed, %d failed\n\n\n", passCount, failCount)
+	fmt.Println("========================================")
+	fmt.Printf("%d passed, %d failed\n\n\n", passCount, failCount)
 }
